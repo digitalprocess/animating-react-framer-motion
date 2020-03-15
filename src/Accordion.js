04a6e29c22c5f2717fe34494730451cd629eae19
frontendista <csdelreal@gmail.com>
2020-03-15 14:46:47 -0500

    keyframes and variants

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 
+const variants = {
+	open: { opacity: 1, height: 'auto' },
+	closed: { opacity: 0, height: 0 }
+}
+
 const Accordion = () => {
 	const [ isToggled, setIsToggled ] = useState(false)
 	return (
@@ -8,17 +13,18 @@ const Accordion = () => {
 			<h2
 				role="button"
 				onClick={() => setIsToggled(!isToggled)}
-				style={{ cursor: 'pointer' }}
+				style={{ cursor: 'pointer', overflow: "hidden" }}
 			>
 				Click Me
 			</h2>
 			<AnimatePresence>
 				{isToggled &&
 					<motion.div
-						initial={{ opacity: 0, height: 0 }}
-						animate={{ opacity: 1, height: 'auto' }}
-						exit={{ opacity: 0, height: 0 }}
 						style={{overflow: 'hidden'}}
+						variants={variants}
+						initial="closed"
+						animate="open"
+						exit="closed"
 					>
 						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis nam possimus quasi laudantium inventore optio porro sunt eveniet quam vel. Aliquid nobis delectus maxime earum commodi corrupti nisi eligendi quaerat!</p>
 					</motion.div>
